01cdc5bede3a82e53242120d6621a8b4cd2188f1
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-07-28 14:44:41 +0300

    Implement the demo

--- demo/Main.hs
+++ demo/Main.hs
@@ -1,8 +1,33 @@
 module Main where
 
 import Rebase.Prelude
+import CompoundTypes.Strict
 
 
 main =
-  undefined
+  error "This demonstration is all about the compilability"
 
+-- |
+-- Same as the following type: 
+-- 
+-- > Sum3 Int Char Bool
+type IntCharBoolSum =
+  Int + Char + Bool
+
+-- |
+-- How it can be pattern-matched
+intCharBoolSumToText :: IntCharBoolSum -> String
+intCharBoolSumToText =
+  \case
+    Sum3_1 int -> "Int: " <> show int
+    Sum3_2 char -> "Char: " <> show char
+    Sum3_3 bool -> "Bool: " <> show bool
+
+-- |
+-- Same as the following type:
+-- 
+-- > Sum3 Int (Product2 Char (Sum2 Bool String)) Char
+-- 
+-- Just as in the math, the product operator exhibits a higher priority.
+type SumAndProductMixture =
+  Int + Char * (Bool + String) + Char
