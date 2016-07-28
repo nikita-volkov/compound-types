f89967ffbe0b1f5ff39fcfc73c2962638326b870
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-07-28 13:58:26 +0300

    Init

---
+++ demo/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import Rebase.Prelude
+
+
+main =
+  undefined
+
---
+++ library/CompoundTypes/Private/Prelude.hs
@@ -0,0 +1,10 @@
+module CompoundTypes.Private.Prelude
+(
+  module Exports,
+)
+where
+
+
+-- base-prelude
+-------------------------
+import BasePrelude as Exports
