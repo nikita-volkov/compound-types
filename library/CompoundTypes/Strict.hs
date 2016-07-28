f5ff9af92f0d315e49f4896dc37f99f569e6af7a
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-07-28 14:04:37 +0300

    Strict types

---
+++ library/CompoundTypes/Strict.hs
@@ -0,0 +1,11 @@
+-- |
+-- Implementations of strict data-structures.
+module CompoundTypes.Strict
+(
+  module CompoundTypes.Private.Strict.Sum,
+  module CompoundTypes.Private.Strict.Product,
+)
+where
+
+import CompoundTypes.Private.Strict.Sum
+import CompoundTypes.Private.Strict.Product
