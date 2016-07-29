9b4a2603fb3f36b98013be32b6f6f05620d10e16
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-07-29 09:52:41 +0300

    Add arities up to 7

--- library/CompoundTypes/Private/Strict/Product.hs
+++ library/CompoundTypes/Private/Strict/Product.hs
@@ -13,6 +13,13 @@ data Product4 v1 v2 v3 v4 =
 data Product5 v1 v2 v3 v4 v5 =
   Product5 !v1 !v2 !v3 !v4 !v5
 
+data Product6 v1 v2 v3 v4 v5 v6 =
+  Product6 !v1 !v2 !v3 !v4 !v5 !v6
+
+data Product7 v1 v2 v3 v4 v5 v6 v7 =
+  Product7 !v1 !v2 !v3 !v4 !v5 !v6 !v7
+
+
 -- |
 -- Automatically derives the product-type of the according arity
 -- from expressions such as:
@@ -23,18 +30,40 @@ data Product5 v1 v2 v3 v4 v5 =
 -- 
 -- > Product3 Int Char Bool
 type family (*) a b where
+  Product6 v1 v2 v3 v4 v5 v6 * v7 =
+    Product7 v1 v2 v3 v4 v5 v6 v7
+  Product5 v1 v2 v3 v4 v5 * Product2 v6 v7 =
+    Product7 v1 v2 v3 v4 v5 v6 v7
+  Product5 v1 v2 v3 v4 v5 * v6 =
+    Product6 v1 v2 v3 v4 v5 v6
+  Product4 v1 v2 v3 v4 * Product3 v5 v6 v7 =
+    Product7 v1 v2 v3 v4 v5 v6 v7
+  Product4 v1 v2 v3 v4 * Product2 v5 v6 =
+    Product6 v1 v2 v3 v4 v5 v6
   Product4 v1 v2 v3 v4 * v5 =
     Product5 v1 v2 v3 v4 v5
+  Product3 v1 v2 v3 * Product4 v4 v5 v6 v7 =
+    Product7 v1 v2 v3 v4 v5 v6 v7
+  Product3 v1 v2 v3 * Product3 v4 v5 v6 =
+    Product6 v1 v2 v3 v4 v5 v6
   Product3 v1 v2 v3 * Product2 v4 v5 =
     Product5 v1 v2 v3 v4 v5
   Product3 v1 v2 v3 * v4 =
     Product4 v1 v2 v3 v4
+  Product2 v1 v2 * Product5 v3 v4 v5 v6 v7 =
+    Product7 v1 v2 v3 v4 v5 v6 v7
+  Product2 v1 v2 * Product4 v3 v4 v5 v6 =
+    Product6 v1 v2 v3 v4 v5 v6
   Product2 v1 v2 * Product3 v3 v4 v5 =
     Product5 v1 v2 v3 v4 v5
   Product2 v1 v2 * Product2 v3 v4 =
     Product4 v1 v2 v3 v4
   Product2 v1 v2 * v3 =
     Product3 v1 v2 v3
+  v1 * Product6 v2 v3 v4 v5 v6 v7 =
+    Product7 v1 v2 v3 v4 v5 v6 v7
+  v1 * Product5 v2 v3 v4 v5 v6 =
+    Product6 v1 v2 v3 v4 v5 v6
   v1 * Product4 v2 v3 v4 v5 =
     Product5 v1 v2 v3 v4 v5
   v1 * Product3 v2 v3 v4 =
--- library/CompoundTypes/Private/Strict/Sum.hs
+++ library/CompoundTypes/Private/Strict/Sum.hs
@@ -13,6 +13,13 @@ data Sum4 v1 v2 v3 v4 =
 data Sum5 v1 v2 v3 v4 v5 =
   Sum5_1 !v1 | Sum5_2 !v2 | Sum5_3 !v3 | Sum5_4 !v4 | Sum5_5 !v5
 
+data Sum6 v1 v2 v3 v4 v5 v6 =
+  Sum6_1 !v1 | Sum6_2 !v2 | Sum6_3 !v3 | Sum6_4 !v4 | Sum6_5 !v5 | Sum6_6 !v6
+
+data Sum7 v1 v2 v3 v4 v5 v6 v7 =
+  Sum7_1 !v1 | Sum7_2 !v2 | Sum7_3 !v3 | Sum7_4 !v4 | Sum7_5 !v5 | Sum7_6 !v6 | Sum7_7 !v7
+
+
 -- |
 -- Automatically derives the sum-type of the according arity
 -- from expressions such as:
@@ -23,18 +30,40 @@ data Sum5 v1 v2 v3 v4 v5 =
 -- 
 -- > Sum3 Int Char Bool
 type family (+) a b where
+  Sum6 v1 v2 v3 v4 v5 v6 + v7 =
+    Sum7 v1 v2 v3 v4 v5 v6 v7
+  Sum5 v1 v2 v3 v4 v5 + Sum2 v6 v7 =
+    Sum7 v1 v2 v3 v4 v5 v6 v7
+  Sum5 v1 v2 v3 v4 v5 + v6 =
+    Sum6 v1 v2 v3 v4 v5 v6
+  Sum4 v1 v2 v3 v4 + Sum3 v5 v6 v7 =
+    Sum7 v1 v2 v3 v4 v5 v6 v7
+  Sum4 v1 v2 v3 v4 + Sum2 v5 v6 =
+    Sum6 v1 v2 v3 v4 v5 v6
   Sum4 v1 v2 v3 v4 + v5 =
     Sum5 v1 v2 v3 v4 v5
+  Sum3 v1 v2 v3 + Sum4 v4 v5 v6 v7 =
+    Sum7 v1 v2 v3 v4 v5 v6 v7
+  Sum3 v1 v2 v3 + Sum3 v4 v5 v6 =
+    Sum6 v1 v2 v3 v4 v5 v6
   Sum3 v1 v2 v3 + Sum2 v4 v5 =
     Sum5 v1 v2 v3 v4 v5
   Sum3 v1 v2 v3 + v4 =
     Sum4 v1 v2 v3 v4
+  Sum2 v1 v2 + Sum5 v3 v4 v5 v6 v7 =
+    Sum7 v1 v2 v3 v4 v5 v6 v7
+  Sum2 v1 v2 + Sum4 v3 v4 v5 v6 =
+    Sum6 v1 v2 v3 v4 v5 v6
   Sum2 v1 v2 + Sum3 v3 v4 v5 =
     Sum5 v1 v2 v3 v4 v5
   Sum2 v1 v2 + Sum2 v3 v4 =
     Sum4 v1 v2 v3 v4
   Sum2 v1 v2 + v3 =
     Sum3 v1 v2 v3
+  v1 + Sum6 v2 v3 v4 v5 v6 v7 =
+    Sum7 v1 v2 v3 v4 v5 v6 v7
+  v1 + Sum5 v2 v3 v4 v5 v6 =
+    Sum6 v1 v2 v3 v4 v5 v6
   v1 + Sum4 v2 v3 v4 v5 =
     Sum5 v1 v2 v3 v4 v5
   v1 + Sum3 v2 v3 v4 =
